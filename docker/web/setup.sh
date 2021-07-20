d70ef23e8c743e0e0fe895be314558ca8b92dbef
15960077875 <15960077875@139.com>
2021-07-20 11:22:20 +0800

    增加docker web上传题库时nginx的大小限制

--- docker/web/setup.sh
+++ docker/web/setup.sh
@@ -12,3 +12,10 @@ chown -R www-data:www-data /home/judge/src/web
 # Adjust system configuration
 cp /home/judge/src/install/default.conf  /etc/nginx/sites-available/default
 sed -i "s#127.0.0.1:9000#unix:/var/run/php/php7.2-fpm.sock#g"    /etc/nginx/sites-available/default
+
+
+if grep "client_max_body_size" /etc/nginx/nginx.conf ; then 
+	echo "client_max_body_size already added" ;
+else
+	sed -i "s:include /etc/nginx/mime.types;:client_max_body_size    1024m;\n\tinclude /etc/nginx/mime.types;:g" /etc/nginx/nginx.conf
+fi
